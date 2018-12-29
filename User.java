cc28693a8f1d325db948ebe391b54dba03b5f78b
jothilakshmiR <43263299+jothilakshmiR@users.noreply.github.com>
2018-12-29 18:48:13 +0530

    Add files via upload

---
+++ User.java
@@ -0,0 +1,253 @@
+package com.MyRLDemo.bean;
+
+public class User {
+private  String Name,Address,Fathers_Name,Occupation,Academic_Qualifications,Economical_Status,Social_Status,Previous_Government_or_Private_Exams_Appeared,Marks_Scored_in_the_Exams_Appeared,	
+		Areas_of_Interest,Previous_Working_Experience,Last_Salary_Drawn,Are_you_a_Physically_Challenged_Person,
+		Are_you_a_Widow_or_Divorced_Person,Are_you_an_Ex_servicemen,Have_you_registered_your_Qualifications_in_Employment_Office;
+		
+		
+	private int Age,Contact_Number,Tamil,English,Maths,Science,Social,Marks_Scored_in_HSC;
+
+
+	public String getName() {
+		return Name;
+	}
+
+
+	public void setName(String name) {
+		Name = name;
+	}
+	public int getAge() {
+		return Age;
+	}
+
+
+	public void setAge(int age) {
+		Age = age;
+	}
+
+
+	public String getAddress() {
+		return Address;
+	}
+
+
+	public void setAddress(String address) {
+		Address = address;
+	}
+	public int getContact_Number() {
+		return Contact_Number;
+	}
+
+
+	public void setContact_Number(int contact_Number) {
+		Contact_Number = contact_Number;
+	}
+
+	public String getFathers_Name() {
+		return Fathers_Name;
+	}
+
+
+	public void setFathers_Name(String fathers_Name) {
+		Fathers_Name = fathers_Name;
+	}
+
+
+	public String getOccupation() {
+		return Occupation;
+	}
+
+
+	public void setOccupation(String occupation) {
+		Occupation = occupation;
+	}
+	public int getTamil() {
+		return Tamil;
+	}
+
+
+	public void setTamil(int tamil) {
+		Tamil = tamil;
+	}
+
+
+	public int getEnglish() {
+		return English;
+	}
+
+
+	public void setEnglish(int english) {
+		English = english;
+	}
+
+
+	public int getMaths() {
+		return Maths;
+	}
+
+
+	public void setMaths(int maths) {
+		Maths = maths;
+	}
+
+
+	public int getScience() {
+		return Science;
+	}
+
+
+	public void setScience(int science) {
+		Science = science;
+	}
+
+
+	public int getSocial() {
+		return Social;
+	}
+
+
+	public void setSocial(int social) {
+		Social = social;
+	}
+
+
+	public int getMarks_Scored_in_HSC() {
+		return Marks_Scored_in_HSC;
+	}
+
+
+	public String getAcademic_Qualifications() {
+		return Academic_Qualifications;
+	}
+
+
+	public void setAcademic_Qualifications(String academic_Qualifications) {
+		Academic_Qualifications = academic_Qualifications;
+	}
+
+
+	public String getEconomical_Status() {
+		return Economical_Status;
+	}
+
+
+	public void setEconomical_Status(String economical_Status) {
+		Economical_Status = economical_Status;
+	}
+
+
+	public String getSocial_Status() {
+		return Social_Status;
+	}
+
+
+	public void setSocial_Status(String social_Status) {
+		Social_Status = social_Status;
+	}
+
+
+	public String getPrevious_Government_or_Private_Exams_Appeared() {
+		return Previous_Government_or_Private_Exams_Appeared;
+	}
+
+
+	public void setPrevious_Government_or_Private_Exams_Appeared(String previous_Government_or_Private_Exams_Appeared) {
+		Previous_Government_or_Private_Exams_Appeared = previous_Government_or_Private_Exams_Appeared;
+	}
+
+
+	public String getMarks_Scored_in_the_Exams_Appeared() {
+		return Marks_Scored_in_the_Exams_Appeared;
+	}
+
+
+	public void setMarks_Scored_in_the_Exams_Appeared(String marks_Scored_in_the_Exams_Appeared) {
+		Marks_Scored_in_the_Exams_Appeared = marks_Scored_in_the_Exams_Appeared;
+	}
+
+
+	public String getAreas_of_Interest() {
+		return Areas_of_Interest;
+	}
+
+
+	public void setAreas_of_Interest(String areas_of_Interest) {
+		Areas_of_Interest = areas_of_Interest;
+	}
+
+
+	public String getPrevious_Working_Experience() {
+		return Previous_Working_Experience;
+	}
+
+
+	public void setPrevious_Working_Experience(String previous_Working_Experience) {
+		Previous_Working_Experience = previous_Working_Experience;
+	}
+
+
+	public String getLast_Salary_Drawn() {
+		return Last_Salary_Drawn;
+	}
+
+
+	public void setLast_Salary_Drawn(String last_Salary_Drawn) {
+		Last_Salary_Drawn = last_Salary_Drawn;
+	}
+
+
+	public String getAre_you_a_Physically_Challenged_Person() {
+		return Are_you_a_Physically_Challenged_Person;
+	}
+
+
+	public void setAre_you_a_Physically_Challenged_Person(String are_you_a_Physically_Challenged_Person) {
+		Are_you_a_Physically_Challenged_Person = are_you_a_Physically_Challenged_Person;
+	}
+
+
+	public String getAre_you_a_Widow_or_Divorced_Person() {
+		return Are_you_a_Widow_or_Divorced_Person;
+	}
+
+
+	public void setAre_you_a_Widow_or_Divorced_Person(String are_you_a_Widow_or_Divorced_Person) {
+		Are_you_a_Widow_or_Divorced_Person = are_you_a_Widow_or_Divorced_Person;
+	}
+
+
+	public String getAre_you_an_Ex_servicemen() {
+		return Are_you_an_Ex_servicemen;
+	}
+
+
+	public void setAre_you_an_Ex_servicemen(String are_you_an_Ex_servicemen) {
+		Are_you_an_Ex_servicemen = are_you_an_Ex_servicemen;
+	}
+
+
+	public String getHave_you_registered_your_Qualifications_in_Employment_Office() {
+		return Have_you_registered_your_Qualifications_in_Employment_Office;
+	}
+
+
+	public void setHave_you_registered_your_Qualifications_in_Employment_Office(
+			String have_you_registered_your_Qualifications_in_Employment_Office) {
+		Have_you_registered_your_Qualifications_in_Employment_Office = have_you_registered_your_Qualifications_in_Employment_Office;
+	}
+
+
+
+	
+
+
+	
+
+
+	public void setMarks_Scored_in_HSC(int marks_Scored_in_HSC) {
+		Marks_Scored_in_HSC = marks_Scored_in_HSC;
+	}
+	
+
+}
